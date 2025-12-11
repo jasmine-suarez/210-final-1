a8c1672dc2d1efe7cca5982f0f69a937577dce68
Jasmine <jazzyysuarez@gmail.com>
2025-12-10 19:13:16 -0800

    update

--- main.cpp
+++ main.cpp
@@ -2,10 +2,22 @@
 // IDE used: VS Code
 
 #include <iostream>
+#include <fstream>
+#include <map>
 using namespace std;
 
 int main() {
+    map<string, int> traffic;
 
-    
+    ifstream fin;
+    fin.open("210-final-1-FA25.txt");
+    if (!fin.good()) {
+        cout << "Error! File not found. Exiting program...\n";
+        return 0;
+    }
+
+    string origin, destination;
+    while (fin >> origin >> destination) {
+
     return 0;
 }
